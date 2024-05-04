3ed055409f7d8a98a64b1e64c7a7f54fe8d31ce2
Mohamed Gueye <mohamed.gueye88@gmail.com>
2024-05-04 14:15:21 -0400

    final commit

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ This is a straightforward dice game implemented in JavaScript. Players refresh t
 
 ### How to Play
 1. Open the HTML file containing the game in a web browser.
-2. Refresh the page to roll the dice.
+2. CLick on the button to generate a new dice image.
 3. The dice will roll automatically, displaying a random number from 1 to 6 for each player.
 4. The game will determine the winner based on which player rolled the higher number.
 5. The result (winner or draw) will be displayed above the dice images.
--- index.js
+++ index.js
@@ -1,49 +1,19 @@
-var randomNumber1 = Math.floor(Math.random() * 6) + 1 ;
-var randomNumber2 = Math.floor(Math.random() * 6) + 1 ;
+// Select the button
+document.querySelector("button").addEventListener("click", function() {
 
+  // Generate new random values
+  var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+  var randomNumber2 = Math.floor(Math.random() * 6) + 1;
 
-if (randomNumber1 > randomNumber2) 
-  document.querySelector("h1").innerHTML = "Player 1 Wins!";
-else if (randomNumber1 < randomNumber2)
-  document.querySelector("h1").innerHTML = "Player 2 Wins!";
-else  
-  document.querySelector("h1").innerHTML = "Draw!";
+  // Update the page
+  if (randomNumber1 > randomNumber2) 
+    document.querySelector("h1").innerHTML = "Player 1 Wins!";
+  else if (randomNumber1 < randomNumber2)
+    document.querySelector("h1").innerHTML = "Player 2 Wins!";
+  else  
+    document.querySelector("h1").innerHTML = "Draw!";
 
-if (randomNumber1 === 1) {
-  document.querySelector(".img1").setAttribute("src", "images/dice1.png");
-}
-else if (randomNumber1 === 2) {
-  document.querySelector(".img1").setAttribute("src", "images/dice2.png");
-}
-else if (randomNumber1 === 3) {
-  document.querySelector(".img1").setAttribute("src", "images/dice3.png");
-}
-else if (randomNumber1 === 4) {
- document.querySelector(".img1").setAttribute("src", "images/dice4.png");
-    }
-else if (randomNumber1 === 5) {
-  document.querySelector(".img1").setAttribute("src", "images/dice5.png");
-}
-else {
-  document.querySelector(".img1").setAttribute("src", "images/dice6.png");
-}
-
-
-if (randomNumber2 === 1) {
-  document.querySelector(".img2").setAttribute("src", "images/dice1.png");
-}
-else if (randomNumber2 === 2) {
-  document.querySelector(".img2").setAttribute("src", "images/dice2.png");
-}
-else if (randomNumber2 === 3) {
-  document.querySelector(".img2").setAttribute("src", "images/dice3.png");
-}
-else if (randomNumber2 === 4) {
- document.querySelector(".img2").setAttribute("src", "images/dice4.png");
-    }
-else if (randomNumber2 === 5) {
-  document.querySelector(".img2").setAttribute("src", "images/dice5.png");
-}
-else {
-  document.querySelector(".img2").setAttribute("src", "images/dice6.png");
-}
+  // Update the image source based on the random number
+  document.querySelector(".img1").setAttribute("src", "images/dice" + randomNumber1 + ".png");
+  document.querySelector(".img2").setAttribute("src", "images/dice" + randomNumber2 + ".png");
+});
